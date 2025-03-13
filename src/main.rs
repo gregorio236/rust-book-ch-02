099c17960409385f38ab17f00e1ad1959cb1370a
gregorio236 <gregorio236@gmail.com>
2025-03-12 23:45:21 -0300

    Add infinite loop for checking

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,26 @@ use std::{cmp::Ordering, io::stdin};
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    stdin().read_line(&mut guess).expect("Failed to read line!");
-
-    let guess: i32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
     let secret_number = random_range(1..=100);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
-    };
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        stdin().read_line(&mut guess).expect("Failed to read line!");
+
+        let guess: i32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+        };
+    }
 }
